ff1791807bf557241b844e13b6634861927b0c82
Axel Roest <axel@phlux.us>
2021-07-23 10:41:41 +0200

    removed ,

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Apptentive",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v13),
     ],
     products: [
         .library(
@@ -18,6 +18,6 @@ let package = Package(
             name: "Apptentive",
             url: "https://github.com/axello/apptentive-ios/releases/download/5.3.3/Apptentive.xcframework.zip",
             checksum: "3aa2d5dab2d7d9e3be62d810452bef930be5817708de9498fb8140e4046d3db1"
-        ),
+        )
     ]
 )
